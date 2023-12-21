2874e533665f65109414993c4b4b16b5433d0d5f
AhmadShamouka <shamoukaahmad@gmail.com>
2023-12-21 20:58:39 +0200

    backend:getOneSurvey

--- backend/controllers/survey.controllers.js
+++ backend/controllers/survey.controllers.js
@@ -3,29 +3,64 @@ const User = require("../models/user.model");
 
 const addSurvey = async (req, res) => {
   const { title, questions } = req.body;
-
   try {
-    const survey = await Survey.create({
-      title,
-      questions,
-      userId: req.user._id,
-    });
-    return res.status(200).send({ survey });
-  } catch (error) {
-    return res.status(500).send(error);
+    if (req.user.role_id === 1) {
+      try {
+        const survey = await Survey.create({
+          title,
+          questions,
+          userId: req.user._id,
+        });
+        return res.status(200).send({ survey });
+      } catch (error) {
+        return res.status(500).send(error);
+      }
+    } else {
+      return res.status(500).send({ error: "You dont have Permission" });
+    }
+  } catch {
+    return res.status(500).send({ error: "Check all Inputs" });
   }
 };
+
 const getSurvey = async (req, res) => {
   try {
-    const surveyData = await Survey.find();
+    if (req.user.role_id === 1) {
+      try {
+        const surveyData = await Survey.find();
+        return res.status(200).send({ surveyData });
+      } catch (error) {
+        console.error("Error fetching survey:", error);
+        return res.status(500).send(error);
+      }
+    } else {
+      return res.status(500).send({ error: "You dont have Permission" });
+    }
+  } catch {
+    return res.status(500).send({ error: "Check all Inputs" });
+  }
+};
+
+const getOneSurvey = async (req, res) => {
+  const surveyId = req.body.surveyId;
+  console.log(`Fetching survey with ID: ${surveyId}`);
+
+  try {
+    const surveyData = await Survey.findOne({ _id: surveyId });
+
+    if (!surveyData) {
+      return res.status(404).send({ error: "Survey not found" });
+    }
 
     return res.status(200).send({ surveyData });
   } catch (error) {
     console.error("Error fetching survey:", error);
-    return res.status(500).send(error);
+    return res.status(500).send({ error: "Internal Server Error" });
   }
 };
+
 module.exports = {
   addSurvey,
   getSurvey,
+  getOneSurvey,
 };
--- backend/routes/survey.routes.js
+++ backend/routes/survey.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { authMiddleware } = require("../middlewares/auth.middelware");
-const { addSurvey, getSurvey } = require("../controllers/survey.controllers");
+const {
+  addSurvey,
+  getSurvey,
+  getOneSurvey,
+} = require("../controllers/survey.controllers");
 const router = express.Router();
 
 router.post("/new", authMiddleware, addSurvey);
-router.get("/getSurvey", getSurvey);
+router.get("/getSurvey", authMiddleware, getSurvey);
+router.post("/getOneSurvey", authMiddleware, getOneSurvey);
 module.exports = router;
